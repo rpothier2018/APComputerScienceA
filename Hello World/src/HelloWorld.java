c8288c18f4e44272dd8adffa18a5918c250cf837
rpothier2018 <rpothier2018@cchsraider.net>
2017-09-21 13:41:07 -0400

    Initial commit

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("                   d            ");
+	System.out.println("             o     l            ");
+	System.out.println("            l     r             ");
+	System.out.println("             l     o            ");
+	System.out.println("            e     W             ");
+	System.out.println("           H                    ");
+	System.out.println("       xxxxxxxxxxxxxx           ");
+	System.out.println("       x            xxxx        ");
+	System.out.println("       x    Java    x   x       ");
+	System.out.println("       x            x   x       ");
+	System.out.println("       x   is hot!  xxxx        ");
+	System.out.println("       x            x           ");
+	System.out.println("       x            x           ");
+	System.out.println("        xxxxxxxxxxxx            ");
+}
+
+}
